a3749ea0b3ca7c33c3fe3750e4aaa0c205fba891
ellen310 <ellen10735@gmail.com>
2024-11-19 01:27:26 +0900

    add BuildScript

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BuildScript
+{
+    //WebGL 빌드
+    [MenuItem("Build/Build WebGL")]
+    public static void PerformBuild()
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+
+        // Scene 추가
+        List<string> scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if(!scene.enabled) continue;
+            scenes.Add(scene.path);
+        }
+        options.scenes = scenes.ToArray();
+
+        // 타겟 경로
+        options.locationPathName = "Builds";
+
+        // 빌드 타겟
+        options.target = BuildTarget.WebGL;
+        
+        // 빌드
+        BuildPipeline.BuildPlayer(options);
+    }
+
+}
